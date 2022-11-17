310dd0e89373d13f9b6dbc640e83d04a0f51ee5e
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-11-17 08:10:22 +0300

    3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -38,16 +38,17 @@ def c(text):
     return 'C {}'.format(par)
 
 
-app.route('/python/(<text>)', strict_slashes=False)
-
+app.route('/python/', strict_slashes=False)
+app.route('/python/<text>', strict_slashes=False)
 
 def python(text='is cool'):
     """
     display “Python ”, followed by the value of the text
     variable (replace underscore _ symbols with a space )
     """
-    par = text.replace('_', '')
-    return 'Python {}'.format(par)
+    if text is not 'is cool':
+        par = text.replace('_', '')
+        return 'Python {:s}'.format(par)
 
 
 if __name__ == '__main__':
